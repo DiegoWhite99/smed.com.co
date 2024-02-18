cf8ae3790b71e658997068367f9975834f3729dd
DiegoWhite99 <diegofer.cas.99@gmail.com>
2024-02-18 18:30:22 -0500

    se grega JS y cambios de forma

---
+++ JS/Functions.js
@@ -0,0 +1,36 @@
+var a = document.getElementById("loginbtn");
+var b = document.getElementById("registerbtn");
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+
+/* ---------------------------> apartado de funciones <----------------------- */
+
+function login() {
+    x.style.left = "20px";
+    y.style.right = "-520px";
+    a.className += " white-btn";
+    b.className = "btn";
+    x.style.opacity  = 1;
+    y.style.opacity  = 0;
+}
+
+function register(){
+    x.style.left = "-510px";
+    y.style.right = "5px";
+    a.className = "btn";
+    b.className += " white-btn";
+    x.style.opacity  = 0;
+    y.style.opacity  = 1;
+}
+
+/* ---------------------------> apartado de resposive <----------------------- */
+
+function MyMenuFuction(){
+    var i = document.getElementById("navMenu");
+
+    if(i.className === "nav-menu") {
+        i.className += "resposive";
+    } else {
+        i.className = "nav-menu";
+    }
+}
