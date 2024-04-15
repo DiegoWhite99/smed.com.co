5279ad2619c7ad87684e1c8b70c756db434604f0
Crisstiv <160662456+Crisstiv@users.noreply.github.com>
2024-04-15 09:47:36 -0500

    Update validar.php

--- DB/validar.php
+++ DB/validar.php
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo '
             <script>
             alert("Registro Exitoso");
-            window.location = "index.html";
+            window.location = "login.html";
         </script>
 
         ';
